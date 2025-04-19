444c268da8029dea4344b38de41a8c9b562a8249
Vasu <vasudhiman105@gmail.com>
2025-04-19 15:32:24 +0530

    Remove : from path

--- backend/index.js
+++ backend/index.js
@@ -18,13 +18,13 @@ app.use(bodyParser.json())
 app.use(cookieParser())
 app.use(cors({ origin:'http://localhost:5173', credentials:true } ))
 
-app.use('/:api/auth' , authRoutes);
-app.use('/:api/messages' , messageRoutes);
+app.use('/api/auth' , authRoutes);
+app.use('/api/messages' , messageRoutes);
 
 if(process.env.NODE_ENV === "production"){
     app.use(express.static(path.join(__dirname, "../frontend/dist")))
 
-    app.get("/*all" , (req,res) => {
+    app.get("*" , (req,res) => {
         res.sendFile(path.join(__dirname , "../frontend" , "dist" , "index.html"))
     })
 }
